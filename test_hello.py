efef4aca7f5d9c870416f42b5fc57c7f971e2f39
David Wertheimer <werthds@gmail.com>
2024-03-07 23:17:30 +0000

    add a test to add negative numbers

--- test_hello.py
+++ test_hello.py
@@ -4,3 +4,7 @@ from hello import add
 def test_add():
     """test adding 1 plus 2 and expect 3"""
     assert add(1, 2) == 3
+
+def test_add_negatives():
+    """test adding -1 plus -2 expect -3"""
+    assert add(-1, -2) == -3
